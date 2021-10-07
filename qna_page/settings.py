d0d820bb133582f66e01831d97cade7225f93d24
DevLeti <devleti0106@gmail.com>
2021-10-07 14:11:32 +0000

    EDIT setting.py : added ALLOWED_HOSTS .pythonanywhere.com

--- qna_page/settings.py
+++ qna_page/settings.py
@@ -119,4 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-ALLOWED_HOSTS = ['127.0.0.1', 'djangotestdevleti.run.goorm.io']
+ALLOWED_HOSTS = ['127.0.0.1', 'djangotestdevleti.run.goorm.io', '.pythonanywhere.com']

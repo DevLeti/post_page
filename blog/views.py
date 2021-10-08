12bbc3822c3ed149ea414ec5bb621dcf73b8f321
DevLeti <devleti0106@gmail.com>
2021-10-08 13:54:37 +0000

    EDIT blog/views.py

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.utils import timezone
-from models import Post
+from .models import Post
 
 # Create your views here.
 def post_list(request): # render 메서드를 호출, blog/post_list.html을 보여줌

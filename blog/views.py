26d10719c7a1b5b7d69c7645be801868033a8560
DevLeti <devleti0106@gmail.com>
2021-10-08 13:35:44 +0000

    EDIT blogs/views.py, qna_page/urls.py, ADD blog/templates/urls.py

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def post_list(request): # render 메서드를 호출, blog/post_list.html을 보여줌
+    return render(request, 'blog/post_list.html', {})

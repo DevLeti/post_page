16636741193351e32a41f0ca6f2675bc46fb221b
DevLeti <devleti0106@gmail.com>
2021-10-09 14:32:00 +0000

    Renamed qna_page -> post_page

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qna_page.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'post_page.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:

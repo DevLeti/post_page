d2d1353d35c2cd72404b5735992d1d2cc89b866a
DevLeti <devleti0106@gmail.com>
2022-03-27 01:05:25 +0900

    hidden SECRET_KEY

--- post_page/settings.py
+++ post_page/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'cv5nxld$awnvz%rb8esjk#rlvue)*6oh24a%@#afxk!s!jd4f!'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
